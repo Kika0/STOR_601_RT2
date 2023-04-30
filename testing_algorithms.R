e21ddd93e8a7c82cdb4e7c2d6d6855767d623cf1
Bratkova <bratkova@lancaster.ac.uk>
2023-04-30 22:33:08 +0100

    Test NSGS on random Normal samples with diff mean

--- testing_algorithms.R
+++ testing_algorithms.R
@@ -5,5 +5,10 @@ source("NSGS.R")
 
 # NSGS ----
 # From Wilcox (1984) table we have h(n_0=20,k=5,1-a/2=0.975)=3.4
-NSGS(a=0.05,n_0=20,k=5,h=3.4,IZ=0.1)
+NSGS(a=0.05,IZ=0.1,n_0=20,h=3.4,k=5)
 
+# perform 1000 times
+nsgs <- c()
+for (i in 1:1000) {
+  nsgs[i] <- NSGS(a=0.05,IZ=0.1,n_0=20,h=3.4,k=5)
+}
