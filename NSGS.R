54cf67d2792224955a45455f5402527cf29f4411
Bratkova <bratkova@lancaster.ac.uk>
2023-05-02 21:20:00 +0100

    Add replication parameter for macroreplications

--- NSGS.R
+++ NSGS.R
@@ -1,6 +1,6 @@
 # NSGS algorithm
 
-NSGS <- function(a,IZ,n_0,h,k) {
+NSGS <- function(a,IZ,n_0,h,k,replication=1) {
   t <- qt(c((1-a/2)^(1/(k-1))), df=n_0-1)
 # initiate placeholder for Y_i(x_i)
   Y <- data.frame(matrix(ncol=k,nrow = 0))
@@ -18,7 +18,7 @@ NSGS <- function(a,IZ,n_0,h,k) {
   # simulate each feasible solution n_0 times 
   for (i in 1:k) {
     for (j in 1:n_0) {
-    Y[j,i] <- s_S_int(i=i,seed=j)
+    Y[j,i] <- s_S_int(i=i,seed=(j+1000*replication) )
   }
     # calculate the sample mean
     Y_bar[i] <- mean(Y[1:n_0,i])
@@ -51,7 +51,7 @@ NSGS <- function(a,IZ,n_0,h,k) {
       N[i] <- max(n_0,ceiling((h/IZ)^2*s_2[i]))
       # simulate (N_i-n_0)-times
       for (j in (n_0+1):N[i]) {
-        Y[j,i] <- s_S_int(i=i,seed=j)
+        Y[j,i] <- s_S_int(i=i,seed=(j + 1000*replication) )
     #  Y[(n_0+1):N[i],i] <- rnorm(n=(N[i]-n_0),mean=0.1*i,sd=1)
       }
       # calculate the sample mean
