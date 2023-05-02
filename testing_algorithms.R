bef812378974ee65fa292bd864f79c2ac06f0af3
Bratkova <bratkova@lancaster.ac.uk>
2023-05-02 11:54:53 +0100

    Test macroreplications with NSGS

--- testing_algorithms.R
+++ testing_algorithms.R
@@ -6,12 +6,13 @@ source("s_S_inventory.R")
 
 # NSGS ----
 # From Wilcox (1984) table we have h(n_0=20,k=5,1-a/2=0.975)=3.4
-NSGS(a=0.05,IZ=0.1,n_0=20,h=3.4,k=5)
+# alternatively, for n_0=10, h(n_0=10,...)=3.174
+NSGS(a=0.05,IZ=0.1,n_0=10,h=3.174,k=5)
 
-# perform 1000 times
-nsgs <- c()
-for (i in 1:1000) {
-  nsgs[i] <- NSGS(a=0.05,IZ=0.1,n_0=20,h=3.4,k=5)[[1]]
+# perform 50 times
+nsgs <- list()
+for (i in 1:2) {
+  nsgs[[i]] <- NSGS(a=0.05,IZ=0.1,n_0=10,h=3.174,k=5)
 }
 
 # example of (s,S) inventory system
@@ -28,5 +29,11 @@ five_sols <- data.frame(i=i,s=s,S=S,E_Y=E_Y)
 for (i in 1:5) {
  E_Y[i] <- mean(s_S(s=five_sols[i,2],S=five_sols[i,3],n=1000000,RandomSeed = 1))
 }
-five_sols <- data.frame(i=i,s=s,S=S,E_Y=E_Y)
+five_sols <- data.frame(i=1:5,s=s,S=S,E_Y=E_Y)
+
+# define s_S inventory function that only takes solution integer index i as input
+s_S_int <- function(i,seed) {
+  s_S(s=five_sols[i,2],five_sols[i,3],n=1,RandomSeed = seed)
+}
+
 
